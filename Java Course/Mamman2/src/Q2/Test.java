661f8faf71bf4b1851dabc6e8fbd8ea163c49fbd
RavidDayan <dayanravid@gmail.com>
2023-04-10 20:19:27 +0300

    moved test to package 2

--- Java Course/Mamman2/src/Q2/Test.java
+++ Java Course/Mamman2/src/Q2/Test.java
@@ -1,11 +1,7 @@
-package Q1;
-
-import Q2.BigInt;
-
 public class Test {
 
 	public static void main(String[] args) {
-        String number="-0923840923";
+        String number="0923840923";
         BigInt num=new BigInt(number);
         num.print();
     }
