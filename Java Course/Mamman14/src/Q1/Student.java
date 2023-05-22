6ba1a96f1e57a75ac75761e59465110013daf529
RavidDayan <dayanravid@gmail.com>
2023-05-22 04:05:48 +0300

    finished Student class explanations

--- Java Course/Mamman14/src/Q1/Student.java
+++ Java Course/Mamman14/src/Q1/Student.java
@@ -1,11 +1,11 @@
 package Q1;
 
 public class Student implements Comparable<Student> {
-    private String name;
-    private int id;
-    private int grade;
+    private String name;//students name
+    private int id;//students id
+    private int grade;//students grade
 
-    public Student(String name, int id, int grade) throws IlegalIdException, IlegalGradeException {
+    public Student(String name, int id, int grade) throws IlegalIdException, IlegalGradeException {//creates a new student only if id and grade are legal
         if (!isValidId(id)) {
             throw new IlegalIdException();
         }
@@ -18,7 +18,7 @@ public class Student implements Comparable<Student> {
     }
 
     @Override
-    public int compareTo(Student other) {
+    public int compareTo(Student other) {//compares 2 students by their grades, if instance has bigger grade return 1 ,if instance is has smaller grade returns -1 and otherwise they have the same grade and  returns 0;
         if (this.grade > other.grade) {
             return 1;
         }
@@ -28,27 +28,27 @@ public class Student implements Comparable<Student> {
         return 0;
     }
 
-    private boolean isValidId(int id) {
+    private boolean isValidId(int id) {//checks if id is between 0 to 999999999
         return id >= 0 && id <= 999999999;
     }
 
-    private boolean isValidGrade(int grade) {
+    private boolean isValidGrade(int grade) {//checks if grade is between 0-100
         return grade >= 0 && grade <= 100;
     }
+
     @Override
-    public boolean equals(Object other) {
-        if (other instanceof Student ){
-            return this.id == ((Student) other).id && this.name.equals(((Student) other).name);
-        }
-        else{
+    public boolean equals(Object other) {//checks if 2 Students are equals, recieves object inorder to override object equals method
+        if (other instanceof Student) {//checks if object is student, otherwise returns false
+            return this.id == ((Student) other).id && this.name.equals(((Student) other).name);//checks if student is the same by name and id
+        } else {
             return false;
         }
     }
 
     @Override
-    public String toString() {
+    public String toString() {//returns a string with the name,id and grade of student.
         String returnedString;
-        returnedString = "Name: " + name + " Id: " + String.valueOf(id)+ " Grade: " + String.valueOf(grade);
+        returnedString = "Name: " + name + " Id: " + String.valueOf(id) + " Grade: " + String.valueOf(grade);
         return returnedString;
     }
 }
