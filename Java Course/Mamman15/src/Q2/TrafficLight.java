dd5f222269d6751ef21638c2b661d2925a51a0ac
RavidDayan <dayanravid@gmail.com>
2023-06-04 00:41:26 +0300

    finished traffic light class

--- Java Course/Mamman15/src/Q2/TrafficLight.java
+++ Java Course/Mamman15/src/Q2/TrafficLight.java
@@ -7,12 +7,14 @@ import javafx.scene.shape.Shape;
 
 import static java.lang.Thread.sleep;
 
+//class that represents a single traffic light
 public class TrafficLight implements Runnable {
     private Circle redCircleLight;
     private Circle greenCircleLight;
     private Rectangle redRectangleLight;
     private Rectangle greenRectangleLight;
 
+    //constructor,
     public TrafficLight(Circle redCircle, Circle greenCircle, Rectangle redRectangle, Rectangle greenRectangle) {
         redCircleLight = redCircle;
         greenCircleLight = greenCircle;
@@ -20,6 +22,7 @@ public class TrafficLight implements Runnable {
         greenRectangleLight = greenRectangle;
     }
 
+    //changes traffic lights to show a green light for cars and red for pedestrians
     public void carsGreenLight() {
         colorShapeGreen(greenCircleLight);
         hideShapeColor(redCircleLight);
@@ -27,6 +30,7 @@ public class TrafficLight implements Runnable {
         hideShapeColor(greenRectangleLight);
     }
 
+    //changes traffic lights to show a ref light for cars and green for pedestrians
     public void carsRedLight() {
         colorShapeGreen(greenRectangleLight);
         hideShapeColor(redRectangleLight);
@@ -34,18 +38,22 @@ public class TrafficLight implements Runnable {
         hideShapeColor(greenCircleLight);
     }
 
+    //sets a light color to transparent
     public void hideShapeColor(Shape shape) {
         shape.setFill(Color.TRANSPARENT);
     }
 
+    //sets a light color to red
     public void colorShapeRed(Shape shape) {
         shape.setFill(Color.RED);
     }
 
+    //sets a light color to green
     public void colorShapeGreen(Shape shape) {
         shape.setFill(Color.GREEN);
     }
 
+    //changes the green pedestrian light off and on when it green
     @Override
     public void run() {
         colorShapeGreen(greenRectangleLight);
