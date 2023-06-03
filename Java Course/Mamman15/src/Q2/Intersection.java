171af9ef636b781d6c498acbe5a26d81f72090ff
RavidDayan <dayanravid@gmail.com>
2023-06-04 00:49:09 +0300

    finished intersection class

--- Java Course/Mamman15/src/Q2/Intersection.java
+++ Java Course/Mamman15/src/Q2/Intersection.java
@@ -1,47 +1,51 @@
 package Q2;
 
+//represent an intersection of 4 traffic lights
 public class Intersection implements Runnable {
-    int carGreenTime;
-    int pedestriansGreenTime;
-    TrafficLight top;
-    TrafficLight bottom;
-    TrafficLight left;
-    TrafficLight right;
-    Timer trafficTimer;
+    int topBottomGreenLight;//time for top and bottom green light
+    int topBottomRedLight;//time for top and bottom red light
+    TrafficLight top;//top traffic light
+    TrafficLight bottom;//bottom traffic light
+    TrafficLight left;//left traffic light
+    TrafficLight right;//right traffic light
+    Timer trafficTimer;//light timer of the intersection
 
-    public Intersection(TrafficLight top, TrafficLight bottom, TrafficLight left, TrafficLight right, int carGreenTime, int pedestriansGreenTime) {
+    //constructor
+    public Intersection(TrafficLight top, TrafficLight bottom, TrafficLight left, TrafficLight right, int topBottomGreenLight, int topBottomRedLight) {
         this.top = top;
         this.bottom = bottom;
         this.left = left;
         this.right = right;
-        this.carGreenTime = carGreenTime;
-        this.pedestriansGreenTime = pedestriansGreenTime;
-        this.trafficTimer = new Timer(carGreenTime, pedestriansGreenTime);
+        this.topBottomGreenLight = topBottomGreenLight;
+        this.topBottomRedLight = topBottomRedLight;
+        this.trafficTimer = new Timer(topBottomGreenLight, topBottomRedLight);
     }
 
+    //changes the traffic-lights lights according to that the bottom and top cars are green
     private void topBottomCarGreen() {
         this.left.carsRedLight();
         this.right.carsRedLight();
         this.top.carsGreenLight();
         this.bottom.carsGreenLight();
-        while (trafficTimer.getStatus()) {
+        while (trafficTimer.getStatus()) {//infinite loop to flash the sideway pedestrian lights green until the light time is over
             left.run();
             right.run();
         }
     }
 
+    //changes the traffic-lights lights according to that the side cars are green
     private void sidesCarGreen() {
         this.top.carsRedLight();
         this.bottom.carsRedLight();
         this.left.carsGreenLight();
         this.right.carsGreenLight();
-        while (!trafficTimer.getStatus()) {
+        while (!trafficTimer.getStatus()) {//infinite loop to flash the top and bottom pedestrian lights green until the light time is over
             top.run();
             bottom.run();
         }
-
     }
 
+    //runs the intersection thread that starts a timer for the intersection and switches between top bottom and left right according to set timer for infinite times.
     @Override
     public void run() {
         Thread timer = new Thread(trafficTimer);
