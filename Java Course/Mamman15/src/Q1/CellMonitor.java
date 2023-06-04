53cbf705a0b528747196e6073579a919a0d1d57c
RavidDayan <dayanravid@gmail.com>
2023-06-04 03:50:13 +0300

    finished cell monitor

--- Java Course/Mamman15/src/Q1/CellMonitor.java
+++ Java Course/Mamman15/src/Q1/CellMonitor.java
@@ -1,28 +1,30 @@
 package Q1;
 
+//monitor for cells printing
 public class CellMonitor {
-    boolean[] C;
-    int rowSize;
-    int colSize;
+    boolean[] printedCells;// array of which cell has already printed
+    int rowSize;//rows of original 2d matrix
+    int colSize;//columns of original 2d matrix
 
     public CellMonitor(Cell[][] C) {
         rowSize = C.length;
         colSize = C[0].length;
-        this.C = new boolean[rowSize * colSize + 1];
-        this.C[0] = true;
-        for (int i = 1; i < this.C.length; i++) {
-            this.C[i] = false;
+        this.printedCells = new boolean[rowSize * colSize + 1];
+        this.printedCells[0] = true;//initiater cell so original matrix C[0][0] can print
+        for (int i = 1; i < this.printedCells.length; i++) {
+            this.printedCells[i] = false;
         }
-    }
+
+    }//constructor,sets all printedCells to false except first
 
     public synchronized boolean canCellPrint(int row, int col) {
-        int convertedIndex = 1+(row) * colSize + col;
-        return C[convertedIndex - 1] == true;
-    }
+        int convertedIndex = 1 + (row) * colSize + col;//converts 2D index to 1D index
+        return printedCells[convertedIndex - 1] == true;
+    }//returns true if the cell before the asking cell has printed and false if not
 
     public synchronized void updatePrintedCell(int row, int col) {
-        int convertedIndex = 1+(row) * colSize + col;
-        C[convertedIndex] = true;
-    }
+        int convertedIndex = 1 + (row) * colSize + col;//converts 2D index to 1D index
+        printedCells[convertedIndex] = true;
+    }//updates printCells that the Matrix C[row][col] cell has printed inserts true.
 
 }
