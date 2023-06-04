0bdd1cb081ec6381e886d1741383ddd19c914d03
RavidDayan <dayanravid@gmail.com>
2023-06-04 03:54:42 +0300

    finisheshed Q1

--- Java Course/Mamman15/src/Q1/MatrixMult.java
+++ Java Course/Mamman15/src/Q1/MatrixMult.java
@@ -1,10 +1,11 @@
 package Q1;
 
+//class that receives 2 matrix sizes and prints their multiplication
 public class MatrixMult {
-    int[][] A;
-    int[][] B;
-    CellMonitor monitor;
-    Cell[][] C;
+    int[][] A;//matrix A
+    int[][] B;//matrix B
+    CellMonitor monitor;//matrix C cell monitor
+    Cell[][] C;//matrix c
 
     public MatrixMult(int n, int m, int p) {
         A = new int[n][m];
@@ -13,11 +14,11 @@ public class MatrixMult {
         monitor = new CellMonitor(C);
         randomizedMatrix(A);
         randomizedMatrix(B);
-        printMatrix(A,"A");
-        printMatrix(B,"B");
+        printMatrix(A, "A");
+        printMatrix(B, "B");
         System.out.println("Matrix C");
         calculateMatrixC();
-    }
+    }//constructor and initiates the multiplication process
 
     private void calculateMatrixC() {
         Thread runner;
@@ -28,26 +29,27 @@ public class MatrixMult {
                 runner.start();
             }
         }
-    }
+    }//creates threads for each cell in matrix C and starts each cell's calculating and printing process
 
     private int randomCellValue() {
         return (int) (Math.random() * 10);
-    }
+    }//returns a random cell value between 0-10
 
     private void randomizedMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j]=randomCellValue();
+                matrix[i][j] = randomCellValue();
             }
         }
-    }
-    private void printMatrix(int[][] matrix,String name) {
-        System.out.println("Matrix "+name+":");
+    }//inserts random numbers between 1-10 into each matrix cell
+
+    private void printMatrix(int[][] matrix, String name) {
+        System.out.println("Matrix " + name + ":");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]+" ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-    }
+    }//prints a 2D matrix as a readable matrix
 }
