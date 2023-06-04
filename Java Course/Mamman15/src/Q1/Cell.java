893106793698d18f28362eb3dd4ceb0c911354a1
RavidDayan <dayanravid@gmail.com>
2023-06-04 03:43:42 +0300

    Finshed Cell class

--- Java Course/Mamman15/src/Q1/Cell.java
+++ Java Course/Mamman15/src/Q1/Cell.java
@@ -1,33 +1,33 @@
 package Q1;
 
+//Class the represents a single cell in matrix
 public class Cell implements Runnable {
-    private int cellValue;
-    boolean isPrinted=false;
-    CellMonitor monitor;
+    private int cellValue;//Value of current Cell
+    CellMonitor monitor;//monitor for all the same matrix cells
 
-    private int[][] A;
-    private int[][] B;
-    private int row;
-    private int col;
+    private int[][] A;//original A Matrix
+    private int[][] B;//original B Matrix
+    private int row;//cells row index
+    private int col;//cells col index
 
-    public Cell(int[][] A, int[][] B, int row, int col,CellMonitor monitor) {
+    public Cell(int[][] A, int[][] B, int row, int col, CellMonitor monitor) {
         this.A = A;
         this.B = B;
         this.col = col;
         this.row = row;
-        this.monitor=monitor;
-    }
+        this.monitor = monitor;
+    }//constructor
 
     @Override
     public void run() {
         CalculateCellValue();
-        boolean cantPrintYet=true;
-        while(cantPrintYet){
-            cantPrintYet=!monitor.canCellPrint(row,col);
-        }
+        boolean cantPrintYet = true;
+        while (cantPrintYet) {
+            cantPrintYet = !monitor.canCellPrint(row, col);
+        }//checks if cell before printed until cell before has printed
         printCell();
-        monitor.updatePrintedCell(row,col);
-    }
+        monitor.updatePrintedCell(row, col);
+    }//calculates a cells value ,checks if the cell before it has already printed, prints itself only after cell already printed
 
     private void CalculateCellValue() {
         int[] rowA = getRow();
@@ -35,11 +35,12 @@ public class Cell implements Runnable {
         for (int i = 0; i < rowA.length; i++) {
             cellValue += rowA[i] * columnB[i];
         }
-    }
+
+    }//calculates value of cell by using matrix cell multiplication(row x col)
 
     private int[] getRow() {
         return A[row];
-    }
+    }//returns the cells row of multiplication from first matrix
 
     private int[] getCol() {
         int[] columnArray = new int[B.length];
@@ -47,15 +48,15 @@ public class Cell implements Runnable {
             columnArray[row] = B[row][col];
         }
         return columnArray;
-    }
-    private void printCell(){
-        if(col+1==B[0].length){
-            System.out.println(cellValue+" ");
+    }//returns the cells col of multiplication from first second
 
-        }
-        else{
-            System.out.print(cellValue+" ");
+    private void printCell() {
+        if (col + 1 == B[0].length) {
+            System.out.println(cellValue + " ");
+
+        } else {
+            System.out.print(cellValue + " ");
         }
 
-    }
+    }//prints cell value and goes down a line if cell is last in row
 }
