decdd1ec5c41bee293bf9803fcb37307a104d14b
RavidDayan <dayanravid@gmail.com>
2023-06-15 15:55:04 +0300

    finished Game class

--- Java Course/Mamman16/src/Q1/Game.java
+++ Java Course/Mamman16/src/Q1/Game.java
@@ -1,4 +1,17 @@
 package Q1;
 
+import java.util.ArrayList;
+
 public class Game {
+    private ArrayList<Question> questions;
+
+    public Question getQuestion() {
+        int randomQuestionIndex=(int)(Math.random()*questions.size());
+        Question randomQuestion=questions.get(randomQuestionIndex);
+        questions.remove(randomQuestionIndex);
+        return randomQuestion;
+    }
+    public void addQuestion(Question question){
+        questions.add(question);
+    }
 }
