ba354140010f17ae1068cf65e87c50a24765ea04
RavidDayan <dayanravid@gmail.com>
2023-06-21 00:43:26 +0300

    created server,client fxml

--- Java Course/Mamman16/src/Q2/Server.java
+++ Java Course/Mamman16/src/Q2/Server.java
@@ -1,4 +1,20 @@
 package Q2;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Server {
+public class Server extends Application {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Server");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ Java Course/Mamman16/src/Q2/ServerController.java
@@ -0,0 +1,21 @@
+package Q2;
+
+import java.net.MulticastSocket;
+
+public class ServerController {
+    private String groupIp;
+    int port;
+    MulticastSocket socket;
+    public ServerController(){
+        groupIp=null;
+        port=6666;
+    }
+    private void getGroupIp(){
+
+    }
+    private void startSocket(){
+
+    }
+    public void startServer(){
+    }
+}
